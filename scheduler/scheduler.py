26ba268aac3dde2015bf050ade59e9e757b470cd
malogusto <67070181@kmitl.ac.th>
2026-09-15 11:22:11 +0700

    2fix pep8

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -33,6 +33,5 @@ def scheduler():
         time.sleep(max(0.0, next_run - time.monotonic()))
 
 
-
 if __name__ == '__main__':
     scheduler()
--- web/app.py
+++ web/app.py
@@ -52,5 +52,6 @@ def router_detail(ip):
     )
     return render_template("router_detail.html", ip=ip, record=record)
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
